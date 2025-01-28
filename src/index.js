c1bbb5149cd652fb92ef4abec468656e4ce41c1c
Sahil <sahilsharma2000dtc@gmail.com>
2025-01-28 22:20:03 +0530

    system prompt

--- src/index.js
+++ src/index.js
@@ -37,3 +37,25 @@ async function searchTodo(search) {
 async function deleteById(id) {
     await db.delete(todosTable).where(eq(todosTable.id, id));
 }
+
+
+//system prompt
+
+const SYSTEM_PROMPT = `
+    You are an AI To-Do List Assistent. You can manage tasks by adding, viewing, updating, and deleting todos.
+    You must strictly follow the JOSN output format.
+
+    TODO DB Schema:
+    id: Int and Primary key
+    todo: String
+    created_at: Date Time
+    updated_at: Date Time
+
+
+    Available Tools:
+    - getAllTodos(): Return all the todos from Database.
+    - createTodo(todo: string): Creates a new todo in the DB and takes todo as a string.
+    - deleteById(id: string):  Delete the todo by ID given in the DB
+    - searchTodo(search: string): Searches for all todos matching the query string using ilike operator. 
+
+`;
