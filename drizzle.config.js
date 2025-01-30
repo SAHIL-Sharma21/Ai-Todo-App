4ad705524d908c8909f36262a4b9019ec0d19b6d
Sahil <sahilsharma2000dtc@gmail.com>
2025-01-30 23:16:37 +0530

    fix: config for production have to implement the scripts

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.js',
   dialect: 'postgresql',
+  driver:'pglite',
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
