11dd0c3909fa600d23a4036089581576ea9aa264
Sahil <sahilsharma2000dtc@gmail.com>
2025-01-31 14:08:18 +0530

    prod fix: database configuration working fine

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import {drizzle} from 'drizzle-orm/node-postgres';
-import {Pool} from 'pg';
+import pkg from 'pg';
+
+const {Pool} = pkg;
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
